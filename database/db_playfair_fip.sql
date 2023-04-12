16158e7e0e5303f3fa52ebae997c7adcde899899
Mamatha Vaidehi Ramesh <m_ramesh150220@fanshaweonline.ca>
2023-04-11 23:33:40 -0400

    mobile and desktop

--- database/db_playfair_fip.sql
+++ database/db_playfair_fip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 11, 2023 at 02:25 PM
+-- Generation Time: Apr 12, 2023 at 03:31 AM
 -- Server version: 5.7.36
 -- PHP Version: 8.0.13
 
@@ -36,15 +36,16 @@ CREATE TABLE IF NOT EXISTS `newsletters` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='user details from newsletter sign up';
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COMMENT='user details from newsletter sign up';
 
 --
 -- Dumping data for table `newsletters`
 --
 
 INSERT INTO `newsletters` (`id`, `name`, `email`, `status_id`, `created_at`, `updated_at`) VALUES
-(1, 'Mamatha Vaidehi Ramesh', 'mamathavaidehi@gmail.com', 2, '2023-04-11 14:03:44', '2023-04-11 14:03:44'),
-(2, 'NS', 'ns@gmail.com', 2, '2023-04-11 14:08:33', '2023-04-11 14:08:33');
+(3, 'volunteer3', 'volunteer3@gmail.com', 2, '2023-04-11 16:01:48', '2023-04-11 16:01:48'),
+(4, 'tonya', 'to@gmail.com', 2, '2023-04-11 16:06:42', '2023-04-11 16:06:42'),
+(9, 'volunteerone', 'volunteer1@gmail.com', 1, '2023-04-12 03:30:57', '2023-04-12 03:30:57');
 
 -- --------------------------------------------------------
 
@@ -85,17 +86,18 @@ CREATE TABLE IF NOT EXISTS `volunteers` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1 COMMENT='User table contains info through volunteer/contact form';
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1 COMMENT='User table contains info through volunteer/contact form';
 
 --
 -- Dumping data for table `volunteers`
 --
 
 INSERT INTO `volunteers` (`id`, `fname`, `lname`, `email`, `phone`, `status_id`, `created_at`, `updated_at`) VALUES
-(20, 'Mamatha Vaidehi', 'Ramesh', 'mamathavaidehi@gmail.com', '5488811932', 2, '2023-04-11 13:30:54', '2023-04-11 13:30:54'),
-(19, 'Mamatha Vaidehi', 'Ramesh', 'mamathavaidehi@gmail.com', '5488811932', 2, '2023-04-11 13:28:35', '2023-04-11 13:28:35'),
-(18, 'volunteer1', 'huhu', 'volunteer1@gmail.com', '1234567890', 1, '2023-04-11 12:22:37', '2023-04-11 12:22:37'),
-(21, 'Mamatha Vaidehi', 'Ramesh', 'mamathavaidehi@gmail.com', '5488811932', 2, '2023-04-11 13:31:15', '2023-04-11 13:31:15');
+(24, 'Sherine', 'ss', 'sherinemceween@gmail.com', '1234567890', 2, '2023-04-11 18:45:28', '2023-04-11 20:45:10'),
+(25, 'Mamatha Vaidehi', 'Ramesh', 'mamathavaidehi@gmail.com', '5488811932', 1, '2023-04-11 22:41:07', '2023-04-11 22:41:07'),
+(27, 'Ruby', 'Raam', 'rr@gmail.com', '0987654321', 2, '2023-04-12 03:23:46', '2023-04-12 03:23:46'),
+(28, 'Josh', 'Jerrod', 'josh@gmail.com', '1234352897', 2, '2023-04-12 03:24:44', '2023-04-12 03:24:44'),
+(29, 'volunteer', 'one', 'volunteer1@gmail.com', '0987654321', 1, '2023-04-12 03:25:44', '2023-04-12 03:26:08');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
